2738caddbb4cbf30a2ca1f8ff539dd33adebdaca
Brendon Ngo <Brendonn0809@gmail.com>
2025-09-16 14:26:54 +0930

    dVx

--- AIF Game/Assets/Scripts/GameManager.cs
+++ AIF Game/Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI greentext;
     public float score = 0;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        greentext.text = ("Green: " + score);
     }
     public void AddScore()
     {
